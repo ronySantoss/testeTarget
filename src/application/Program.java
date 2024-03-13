2109f0083032fa1ca07560906af6f3f2d01043f6
ronySantoss <contato.roni@gmail.com>
2024-03-13 17:18:54 -0300

    Quinta questao

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,14 +4,21 @@ public class Program {
 
 	public static void main(String[] args) {
 
-	/*  Deixo um interruptor ligado até que a lâmpada esquente, 
-	 * após isso o desligo e aciono outro que eu ainda não tenha tocado e vou até as lâmpadas.
-	 *  A lâmpada que estiver acesa será a relativa ao segundo interruptor acionado, 
-	 * se a lâmpada estiver apagada, porém quente,
-	 * significa que essa lâmpada é relativa ao primeiro interruptor acionado.
-	 *  E por último, caso a lãmpada esteja apagada e fria, significa que não foi ligada,
-	 *  sendo relativa ao interruptor que nunca acionei */
-		
+		System.out.println(inverterCaracteres("Target"));
 	}
 
+	public static String inverterCaracteres(String s) {
+		
+		int i;
+		int n;
+		String aux;
+		aux = "";
+		
+		n = s.length();
+		for(i=(n-1); i>=0; i--) {
+			aux = aux + s.charAt(i);
+		}
+		
+		return aux;
+	}
 }
