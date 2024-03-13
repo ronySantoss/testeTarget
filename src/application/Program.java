e4994aace1fa05374300b1ae674b538ade75575b
ronySantoss <contato.roni@gmail.com>
2024-03-13 16:30:35 -0300

    Primeira questao

---
+++ src/application/Program.java
@@ -0,0 +1,21 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		int i = 13;
+		int k = 0;
+		int soma = 0;
+		
+		while(k < i) {
+			k += 1;
+			soma += k;
+		}
+		
+		System.out.println(soma);
+		
+		//Resposta da primeira questão: 91
+	}
+
+}
